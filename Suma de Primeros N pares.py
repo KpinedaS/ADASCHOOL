ccbf04fe5c666254a8eb502c4efa0c50624364d6
KpinedaS <145940288+KpinedaS@users.noreply.github.com>
2023-09-24 19:21:29 -0600

    Add files via upload
    The first program is about variables and types
    The second program is about the sum of the n even numbers in a range.

---
+++ Suma de Primeros N pares.py
@@ -0,0 +1,10 @@
+print("Digite el primer numero")
+i=int(input())
+print("Digite el numero  final")
+f=int(input())  
+suma=0
+print("** Los numeros pares de este rango son:")
+while i<=f:
+    if i % 2==0:
+       print(i) 
+    i+=1   
---
+++ Variables Python.py
@@ -0,0 +1,33 @@
+# Definicion de Variables
+V_1 =" Mensaje en una variable"
+V_2 = 3.5
+V_3 = 10
+V_4 = True
+
+# Imprimir las Variables
+print(V_1)
+print(V_2)
+print(V_3)
+print(V_4)
+
+#Definir el tipo de Variables
+print( type(V_1))
+print( type(V_2))
+print( type(V_3))
+print( type(V_4))
+
+# Concatenacion de Variables y correccion
+print( "V2-V3")
+print("La suma es:", V_2+V_3)
+
+# Conversiones
+print(int (V_2))
+print (float( V_3))
+print (bool( V_1))
+print (str(V_4))
+
+# En python los enteros no tienen límite, Que Python tiene enteros sin límite se suele mostrar como una de sus
+# bondades, Los float a diferencia de los int tienen unos valores mínimo y máximos que pueden representar. 
+# La mínima precisión es 2.2250738585072014e-308 y la máxima 1.7976931348623157e+308 , pero si no lo crees,
+# lo puedes verificar tu mismo.
+
